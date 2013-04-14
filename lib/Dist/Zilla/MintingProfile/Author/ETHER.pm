d341ca58c5aec146913dbc11d9e076d828edab08
Karen Etheridge <ether@cpan.org>
2013-04-14 20:53:50 +0000

    Import of ETHER/Dist-Zilla-PluginBundle-Author-ETHER-0.001 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-PluginBundle-Author-ETHER
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         ETHER/Dist-Zilla-PluginBundle-Author-ETHER-0.001.tar.gz
    gitpan-cpan-author:       ETHER
    gitpan-cpan-maturity:     released

---
+++ lib/Dist/Zilla/MintingProfile/Author/ETHER.pm
@@ -0,0 +1,81 @@
+use strict;
+use warnings;
+package Dist::Zilla::MintingProfile::Author::ETHER;
+{
+  $Dist::Zilla::MintingProfile::Author::ETHER::VERSION = '0.001';
+}
+BEGIN {
+  $Dist::Zilla::MintingProfile::Author::ETHER::AUTHORITY = 'cpan:ETHER';
+}
+# ABSTRACT: Mint distributions like ETHER does
+
+use Moose;
+with 'Dist::Zilla::Role::MintingProfile::ShareDir';
+
+__PACKAGE__->meta->make_immutable;
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Dist::Zilla::MintingProfile::Author::ETHER - Mint distributions like ETHER does
+
+=head1 VERSION
+
+version 0.001
+
+=head1 SYNOPSIS
+
+    dzil new -P Author::ETHER -p github Foo::Bar
+
+or:
+
+    #!/bin/bash
+    newdist() {
+        local dist=$1
+        local module=`perl -we"print q{$dist} =~ s/-/::/r"`
+        pushd ~/git
+        dzil new -P Author::ETHER -p github $module
+        cd $dist
+    }
+    newdist Foo-Bar
+
+=head1 DESCRIPTION
+
+The new distribution is packaged with L<Dist::Zilla> using
+L<Dist::Zilla::PluginBundle::Author::ETHER>.
+
+Profiles available are:
+
+=over 4
+
+=item *
+
+C<github>
+
+Creates a distribution hosted on L<http://github>, with hooks to determine the
+module version and other metadata from git.
+
+=back
+
+=head1 SUPPORT
+
+Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Dist/Display.html?Name=Dist-Zilla-PluginBundle-Author-ETHER>
+(or L<mailto:bug-Dist-Zilla-PluginBundle-Author-ETHER@rt.cpan.org>).
+I am also usually active on irc, as 'ether' at C<irc.perl.org>.
+
+=head1 AUTHOR
+
+Karen Etheridge <ether@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Karen Etheridge.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
---
+++ lib/Dist/Zilla/PluginBundle/Author/ETHER.pm
@@ -0,0 +1,331 @@
+use strict;
+use warnings;
+package Dist::Zilla::PluginBundle::Author::ETHER;
+{
+  $Dist::Zilla::PluginBundle::Author::ETHER::VERSION = '0.001';
+}
+# git description: ac04044
+
+BEGIN {
+  $Dist::Zilla::PluginBundle::Author::ETHER::AUTHORITY = 'cpan:ETHER';
+}
+# ABSTRACT: A plugin bundle for distributions built by ETHER
+
+use Moose;
+with
+    'Dist::Zilla::Role::PluginBundle::Easy',
+    'Dist::Zilla::Role::PluginBundle::Config::Slicer';
+
+sub mvp_multivalue_args { qw(stopwords) }
+
+has stopwords => (
+    is => 'ro', isa => 'ArrayRef',
+    lazy => 1,
+    default => sub {
+        exists $_[0]->payload->{stopwords} ? $_[0]->payload->{stopwords} : [];
+    },
+);
+
+sub configure
+{
+    my $self = shift;
+
+    $self->add_plugins(
+        # VersionProvider
+        [ 'Git::NextVersion'    => { version_regexp => '^v([\d._]+)(-TRIAL)?$' } ],
+
+        # MetaData
+        'GithubMeta',
+        [ 'AutoMetaResources'   => { 'bugtracker.rt' => 1 } ],
+        [ 'Authority'           => { authority => 'cpan:ETHER' } ],
+        [ 'MetaNoIndex'         => { directory => [ qw(t xt examples) ] } ],
+        [ 'MetaProvides::Package' => { meta_noindex => 1 } ],
+        'MetaConfig',
+        #[ContributorsFromGit]
+
+        # ExecFiles, ShareDir
+        'ExecDir',
+        'ShareDir',
+
+        # Gather Files
+        [ 'Git::GatherDir'      => { exclude_filename => 'LICENSE' } ],
+        (map { [ $_ ] } qw(MetaYAML MetaJSON License Readme Manifest)),
+        [ 'Test::Compile'       => { fail_on_warning => 1, bail_out_on_fail => 1 } ],
+        [ 'Test::CheckDeps'     => { fatal => 1 } ],
+        'NoTabsTests',
+        'EOLTests',
+        'MetaTests',
+        'Test::Version',
+        'Test::CPAN::Changes',
+        'Test::ChangesHasContent',
+        [ 'Test::MinimumVersion' => { max_target_perl => '5.008008' } ],
+        'PodSyntaxTests',
+        'PodCoverageTests',
+        [ 'Test::PodSpelling'   => { stopwords => $self->stopwords } ],
+        #[Test::Pod::LinkCheck]     many outstanding bugs
+        'Test::Pod::No404s',
+
+        # Prune Files
+        'PruneCruft',
+        'ManifestSkip',
+        # (ReadmeAnyFromPod)
+
+        # Munge Files
+        # (Authority)
+        'Git::Describe',
+        'PkgVersion',
+        'PodWeaver',
+        #[%PodWeaver]
+        [ 'NextRelease'         => { format => '%-8V  %{yyyy-MM-dd HH:mm:ss ZZZZ}d (%U)' } ],
+
+        # Register Prereqs
+        # (MakeMaker)
+        'AutoPrereqs',
+        'MinimumPerl',
+
+        # Install Tool
+        [ 'ReadmeAnyFromPod'    => { type => 'markdown', filename => 'README.md', location => 'root' } ],
+        'MakeMaker',
+        'InstallGuide',
+
+        # After Build
+        [ 'CopyFilesFromBuild'  => { copy => 'LICENSE' } ],
+
+        # Test Runner
+        'RunExtraTests',
+
+        # Before Release
+        [ 'Git::Check'          => { allow_dirty => [ qw(README.md LICENSE) ] } ],
+        'Git::CheckFor::MergeConflicts',
+        [ 'Git::CheckFor::CorrectBranch' => { release_branch => 'master' } ],
+        [ 'Git::Remote::Check'  => { remote_branch => 'master' } ],
+        'CheckPrereqsIndexed',
+        'TestRelease',
+        # (ConfirmRelease)
+
+        # Releaser
+        'UploadToCPAN',
+
+        # After Release
+        [ 'Git::Commit'         => { allow_dirty => [ qw(Changes README.md LICENSE) ], commit_msg => '%N-%v%t%n%n%c' } ],
+        [ 'Git::Tag'            => { tag_format => 'v%v%t', tag_message => 'v%v%t' } ],
+        'Git::Push',
+        [ 'InstallRelease'      => { install_command => 'cpanm .' } ],
+
+        # listed late, to allow all other plugins which do BeforeRelease checks to run first.
+        'ConfirmRelease',
+    );
+}
+
+__PACKAGE__->meta->make_immutable;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Dist::Zilla::PluginBundle::Author::ETHER - A plugin bundle for distributions built by ETHER
+
+=head1 VERSION
+
+version 0.001
+
+=head1 SYNOPSIS
+
+In C<dist.ini>:
+
+    [@Author::ETHER]
+
+=head1 DESCRIPTION
+
+This is a L<Dist::Zilla> plugin bundle. It is approximately equivalent to the
+following C<dist.ini> (following the preamble):
+
+    ;;; VersionProvider
+    [Git::NextVersion]
+    version_regexp = ^v([\d._]+)(-TRIAL)?$
+
+
+    ;;; MetaData
+    [GithubMeta]
+    [AutoMetaResources]
+    bugtracker.rt = 1
+
+    [Authority]
+    authority = cpan:ETHER
+
+    [MetaNoIndex]
+    directory = t
+    directory = xt
+    directory = examples
+
+    [MetaProvides::Package]
+    meta_noindex = 1
+
+    [MetaConfig]
+
+
+    ;;; ExecFiles, ShareDir
+    [ExecDir]
+    [ShareDir]
+
+
+    ;;; Gather Files
+    [Git::GatherDir]
+    exclude_filename = LICENSE
+
+    [MetaYAML]
+    [MetaJSON]
+    [License]
+    [Readme]
+    [Manifest]
+
+    [GatherDir::Template / profile.ini]
+    root   = profiles/github/build_templates
+    prefix = profiles/github
+
+    [Test::Compile]
+    fail_on_warning = 1
+    bail_out_on_fail = 1
+
+    [Test::CheckDeps]
+    :version = 0.005
+    fatal = 1
+
+    [NoTabsTests]
+    [EOLTests]
+    [MetaTests]
+    [Test::CPAN::Changes]
+    [Test::ChangesHasContent]
+    [Test::Version]
+
+    [Test::MinimumVersion]
+    :version = 2.000003
+    max_target_perl = 5.008008
+
+    [PodSyntaxTests]
+    [PodCoverageTests]
+    [Test::PodSpelling]
+    [Test::Pod::No404s]
+
+
+    ;;; Munge Files
+    ; (Authority)
+    [Git::Describe]
+    [PkgVersion]
+    [PodWeaver]
+    [NextRelease]
+    :version = 4.300018
+    format = %-8V  %{yyyy-MM-dd HH:mm:ss ZZZZ}d (%U)
+
+
+    ;;; Register Prereqs
+    [AutoPrereqs]
+    [MinimumPerl]
+
+
+    ;;; Install Tool
+    [ReadmeAnyFromPod]
+    type = markdown
+    filename = README.md
+    location = root
+
+    [MakeMaker]
+    [InstallGuide]
+
+
+    ;;; After Build
+    [CopyFilesFromBuild]
+    copy = LICENSE
+
+
+    ;;; TestRunner
+    [RunExtraTests]
+
+
+    ;;; Before Release
+    [Git::Check]
+    allow_dirty = README.md
+    allow_dirty = LICENSE
+
+    [Git::CheckFor::MergeConflicts]
+
+    [Git::CheckFor::CorrectBranch]
+    :version = 0.004
+    release_branch = master
+
+    [Git::Remote::Check]
+    remote_branch = master
+
+    [CheckPrereqsIndexed]
+    [TestRelease]
+    ;(ConfirmRelease)
+
+
+    ;;; Releaser
+    [UploadToCPAN]
+
+
+    ;;; AfterRelease
+    [Git::Commit]
+    allow_dirty = Changes
+    allow_dirty = README.md
+    allow_dirty = LICENSE
+    commit_msg = %N-%v%t%n%n%c
+
+    [Git::Tag]
+    tag_format = v%v%t
+    tag_message = v%v%t
+
+    [Git::Push]
+
+    [InstallRelease]
+    install_command = cpanm .
+
+
+    ; listed late, to allow all other plugins which do BeforeRelease checks to run first.
+    [ConfirmRelease]
+
+=for Pod::Coverage configure mvp_multivalue_args
+
+The distribution's code is assumed to be hosted at github; the version and
+other metadata is derived directly from the local git repository.
+
+=head1 OPTIONS / OVERRIDES
+
+Use V=<version> to override the version of the distribution being built; otherwise the version is
+incremented from the last git tag.
+
+Subs can be considered "covered" for pod coverage tests by adding a directive to pod:
+
+    =for Pod::Coverage foo bar baz
+
+Stopwords for spelling tests can be added with the C<dist.ini> option:
+
+    stopwords = foo
+    stopwords = bar
+
+=head1 NAMING SCHEME
+
+This distribution follows best practices for author-oriented plugin bundles; for more information,
+see L<KENTNL's distribution|Dist::Zilla::PluginBundle::Author::KENTNL/NAMING-SCHEME>.
+
+=head1 SUPPORT
+
+Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Dist/Display.html?Name=Dist-Zilla-PluginBundle-Author-ETHER>
+(or L<mailto:bug-Dist-Zilla-PluginBundle-Author-ETHER@rt.cpan.org>).
+I am also usually active on irc, as 'ether' at C<irc.perl.org>.
+
+=head1 AUTHOR
+
+Karen Etheridge <ether@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Karen Etheridge.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
